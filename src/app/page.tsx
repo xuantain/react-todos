30d0702be3bb4b1c7b1d2dd09304246175948572
Vien Dinh <maxvien@gmail.com>
2025-03-29 04:27:47 +0000

    config tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { classes } from '@/lib/utils';
+
 export default function Home() {
   return (
     <section>
-      <h1 className="bg-white text-white">Home</h1>
+      <h1 className={classes('bg-white text-white')}>Home</h1>
     </section>
   );
 }
