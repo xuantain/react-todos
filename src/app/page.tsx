ac2ab4219f2d2715bf15151eec9b365f8214d576
Vien Dinh <maxvien@gmail.com>
2025-03-29 04:28:09 +0000

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { classes } from '@/lib/utils';
-
 export default function Home() {
   return (
     <section>
-      <h1 className={classes('bg-white text-white')}>Home</h1>
+      <h1>Home</h1>
     </section>
   );
 }
