a861613025acce7976b6e7cc98b93d17c58e3640
Vien Dinh <maxvien@gmail.com>
2025-04-20 09:09:32 +0000

    create app with deves

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, tsconfigPaths } from "deves/vite";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react()],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
