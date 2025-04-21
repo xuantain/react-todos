67d3c83b6227723644ae7cdf532d28f1fee38487
Vien Dinh <maxvien@gmail.com>
2025-04-21 05:26:05 +0000

    update values type for use zod form

--- src/shared/hooks/use-zod-form.ts
+++ src/shared/hooks/use-zod-form.ts
@@ -2,18 +2,20 @@ import { useForm, type UseFormProps } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z, { ZodSchema } from "zod";
 
-export { z };
+type Values<S extends ZodSchema> = z.infer<S>;
 
 interface Props<S extends ZodSchema>
-  extends Omit<UseFormProps<z.infer<S>>, "resolver"> {
+  extends Omit<UseFormProps<Values<S>>, "resolver"> {
   schema: S;
 }
 
 export function useZodForm<S extends ZodSchema>(props: Props<S>) {
-  const form = useForm<z.infer<S>>({
+  const form = useForm<Values<S>>({
     ...props,
     resolver: zodResolver(props.schema),
   });
 
   return form;
 }
+
+export { z };
