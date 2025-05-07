13c363f85fe80432be91196881d4bf0ca9ae49a6
Vien Dinh <maxvien@gmail.com>
2025-05-07 07:22:52 +0000

    auto format

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: { 
-    port: 3000, 
+  server: {
+    port: 3000,
     host: true,
-    allowedHosts: ["todo.go.com"]
+    allowedHosts: ["todo.go.com"],
   },
   plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
