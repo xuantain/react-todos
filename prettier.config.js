781df04328c8c75e4584f03b239722e5ebf1c115
Vien Dinh <maxvien@gmail.com>
2025-05-09 10:13:37 +0000

    format: fix docker vscode ignore

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, globalIgnores } from "deves/prettier";
 
 export default defineConfig(
-  globalIgnores(["package-lock.json", "pnpm-lock.yaml", ".vscode"]),
+  globalIgnores(["package-lock.json", "pnpm-lock.yaml", ".vscode/docker"]),
 );
